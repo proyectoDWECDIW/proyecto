46861c79071dd264d8f27a4c09adab91be94cddc
juangomez94 <juangomezmartin1994@gmail.com>
2017-02-21 12:36:20 +0100

    Día 21 de febrero

--- parte raul/scriptGeneros.js
+++ parte raul/scriptGeneros.js
@@ -1,16 +1,39 @@
+if (window.XMLHttpRequest) {
+  var xhr = new XMLHttpRequest();
+}else if (ActiveXObject("Microsoft.XMLHTTP")){
+  var xhr = ActiveXObject("Microsoft.XMLHTTP")
+}
+
 function listaJuegos(e) {
   $("img.imagenGeneros").addClass("oculto");
   $("p.pGeneros").addClass("oculto");
   $(document.getElementById(e.target.id)).removeClass("oculto");
-  var fotoGenero=$(document.createElement("img")).attr("style", "width:75%;").attr("src", e.target.id + ".png").attr("class", "listaJuegos");
+  $(document.getElementById(e.target.id)).parent().parent().css({left: "30%", position: "absolute"});
+  xhr.onreadystatechange = gestionarDatos;
+  xhr.open('GET', e.target.id + '.txt', true);
+  xhr.send();
+  var fotoGenero=$(document.createElement("img")).css({width:"80%", float: "left"}).attr("src", e.target.id + ".png").attr("class", "listaJuegos");
   $(".fotos").append(fotoGenero);
   $(document.getElementById(e.target.id)).attr("onclick", "listaGeneros(event);");
+}
+
+function gestionarDatos(){
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    texto.innerText = xhr.responseText;
+  }else if(xhr.readyState == 4 && xhr.status != 200) {
+    var str = "Se ha producido el error: " + xhr.status;
+    str += "\n " + xhr.statusText;
+    str += "\n Más información";
+    str += "\n " + xhr.getAllResponseHeaders;
+  }
 }
 
 function listaGeneros (e){
   $("img.imagenGeneros").removeClass("oculto");
   $("p.pGeneros").removeClass("oculto");
   $("img.listaJuegos").addClass("oculto");
+  $("p#texto").addClass("oculto");
   $("button.volver").addClass("oculto");
   $(document.getElementById(e.target.id)).attr("onclick", "listaJuegos(event);");
+  $(document.getElementById(e.target.id)).parent().parent().css({left: "initial", position: "initial"});
 }
