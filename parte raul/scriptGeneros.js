ec6d91baa7c32026912bb618a771cfe0f23a2ff3
raul gimeno huart <raul gimeno huart>
2017-02-21 04:20:00 +0100

    proyectov5

--- parte raul/scriptGeneros.js
+++ parte raul/scriptGeneros.js
@@ -1,3 +1,16 @@
-onload = function () {
-  
+function listaJuegos(e) {
+  $("img.imagenGeneros").addClass("oculto");
+  $("p.pGeneros").addClass("oculto");
+  $(document.getElementById(e.target.id)).removeClass("oculto");
+  var fotoGenero=$(document.createElement("img")).attr("style", "width:75%;").attr("src", e.target.id + ".png").attr("class", "listaJuegos");
+  $(".fotos").append(fotoGenero);
+  $(document.getElementById(e.target.id)).attr("onclick", "listaGeneros(event);");
+}
+
+function listaGeneros (e){
+  $("img.imagenGeneros").removeClass("oculto");
+  $("p.pGeneros").removeClass("oculto");
+  $("img.listaJuegos").addClass("oculto");
+  $("button.volver").addClass("oculto");
+  $(document.getElementById(e.target.id)).attr("onclick", "listaJuegos(event);");
 }
